f7895c71ecaf9ab963a92bdcb032d4f1e6dfebd9
Cade <ccrandall@mymail.mines.edu>
2020-06-16 19:13:21 +0000

    Add JavaDoc comments

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -19,16 +19,14 @@ import java.util.Collections;
 import java.util.ArrayList;
 import java.util.List;
 
-// figure out what everything is 
-// findAttendeeEvents() for conflicts
-// sorted by the start time
-// searched for conflicts
-
-
 public final class FindMeetingQuery {
-  // @param events: all of the events that exist so far 
-  //                each event contains name, time range, and attendees
-  // @param request: MeetingRequest containing event name, duration, and attendees
+
+  /** 
+    * Create a Collection of TimeRange objects where calendar events can fulfill MeetingRequest
+    * @param events: all of the events that exist so far
+    * @param request: MeetingRequest containing event name, duration, and attendees
+    * @return <code>Collection<TimeRange></code> where meetings are appropriate
+    */
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
     ArrayList<TimeRange> requiredEvents = findAttendeeEvents(events, request.getAttendees());
     ArrayList<TimeRange> optionalEvents = findAttendeeEvents(events, request.getOptionalAttendees());
@@ -42,15 +40,23 @@ public final class FindMeetingQuery {
     Collections.sort(optionalEvents, TimeRange.ORDER_BY_START);
     Collections.sort(optionalAndRequiredEvents, TimeRange.ORDER_BY_START);
 
+    // Return the time slots found to include all optional attendees if one is found 
+    // or if there are no required attendee events
     ArrayList<TimeRange> possibleTimes = findPossibleTimes(optionalAndRequiredEvents, request.getDuration());
-    if (possibleTimes.size() != 0) {
+    if (possibleTimes.size() != 0 || requiredEvents.size() == 0) {
       return possibleTimes;
     } else {
+      // If no events satisfy optional attendee schedules, return the timeslots that work for 
+      // mandatory attendees only
       return findPossibleTimes(requiredEvents, request.getDuration());
     }
   }
 
-  // return only the TimeRanges that our attendees are going to to find potential conflicts
+  /**
+    * Determine possible TimeRanges that attendees are scheduled to attend to find potential conflicts
+    * @param events: Collection of Event objects
+    * @param attendees: Collection of Strings of attendee names
+    */
   private ArrayList<TimeRange> findAttendeeEvents(Collection<Event> events, Collection<String> attendees) {
     ArrayList<TimeRange> output = new ArrayList<>();
     for (Event e : events) {
@@ -64,12 +70,17 @@ public final class FindMeetingQuery {
     return output;
   }
 
+  /** 
+    * Private method to determine possible event times
+    * @param existingTimes: ArrayList<TimeRange> of times that have been found from existing events
+    * @param duration: Duration given by the MeetingRequest
+    */
   private ArrayList<TimeRange> findPossibleTimes(ArrayList<TimeRange> existingTimes, long duration) {
     int previousTime = TimeRange.START_OF_DAY;
     ArrayList<TimeRange> possibleTimes = new ArrayList<>();
 
     for (TimeRange currentTime : existingTimes) {
-      // if the difference between this start and the last end is long enough, add to list of times
+      // If the difference between this start and the last end is long enough, add to list of times
       if ((currentTime.start() - previousTime) >= duration) {
         possibleTimes.add(TimeRange.fromStartEnd(previousTime, currentTime.start(), false));
       } 
@@ -81,7 +92,7 @@ public final class FindMeetingQuery {
       } 
     }
     
-    // add meeting time from the end of the last existing meeting to the end of day
+    // Add meeting time from the end of the last existing meeting to the end of day
     if (TimeRange.END_OF_DAY - previousTime > duration) {
       possibleTimes.add(TimeRange.fromStartEnd(previousTime, TimeRange.END_OF_DAY, true));
     }
--- walkthroughs/week-5-tdd/project/src/test/java/com/google/sps/FindMeetingQueryTest.java
+++ walkthroughs/week-5-tdd/project/src/test/java/com/google/sps/FindMeetingQueryTest.java
@@ -395,7 +395,7 @@ public final class FindMeetingQueryTest {
     // Options :       none
 
     Collection<Event> events = Arrays.asList(
-        new Event("Event 1", TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TIME_1000AM, true),
+        new Event("Event 1", TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TIME_1000AM, false),
             Arrays.asList(PERSON_A)),
         new Event ("Event 2", TimeRange.fromStartEnd(TIME_1000AM, TimeRange.END_OF_DAY, true),
             Arrays.asList(PERSON_B)));
